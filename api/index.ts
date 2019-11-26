2918293f7db817a9c09e2258caab6c9f47bb7ccd
Logan McAnsh <logan@mcan.sh>
2019-11-25 19:42:28 -0500

    feat: add whitehorseinn check
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- api/index.ts
+++ api/index.ts
@@ -10,12 +10,24 @@ Sentry.init({
   dsn: 'https://2287c79cc3f9459a9e3d45378510e484@sentry.io/1832768',
 });
 
-const url =
-  'https://campticonderoga.checkfront.com/reserve/?inline=1&date=20191221&options=category_select&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fcampticonderoga.com&1574626041874#';
+const urls = new Map<string, string>([
+  [
+    'thewhitehorseinn.com',
+    'https://thewhitehorseinn.checkfront.com/reserve/?inline=1&category_id=2&date=20191221&options=tabs&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fwww.thewhitehorseinn.com&1574728158411#D20191221',
+  ],
+  [
+    'campticonderoga.com',
+    'https://campticonderoga.checkfront.com/reserve/?inline=1&date=20191221&options=category_select&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fcampticonderoga.com&1574626041874#',
+  ],
+]);
 
 const isDev = process.env.NOW_REGION === 'dev1';
 
 const IglooChecker = async (req: NowRequest, res: NowResponse) => {
+  const url =
+    urls.get(Array.isArray(req.query.url) ? req.query.url[0] : req.query.url) ??
+    'https://campticonderoga.checkfront.com/reserve/?inline=1&date=20191221&options=category_select&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fcampticonderoga.com&1574626041874#';
+
   const now = new Date();
   const lastDay = new Date(2019, 11, 21);
   const date = lastDay.toLocaleDateString('en', {
