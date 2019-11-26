44a633cfe453f1b67fa9bd2e5a976cc58ad4e15d
Logan McAnsh <logan@mcan.sh>
2019-11-25 20:07:02 -0500

    chore: format dates, wait for document to be idle
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@
 import { NowRequest, NowResponse } from '@now/node';
 import { launch } from 'puppeteer-core';
 import * as Sentry from '@sentry/node';
+import { isAfter, format } from 'date-fns';
 
 import { sendText } from '../lib/send-text';
 import { getOptions } from '../lib/get-options';
@@ -10,14 +11,17 @@ Sentry.init({
   dsn: 'https://2287c79cc3f9459a9e3d45378510e484@sentry.io/1832768',
 });
 
+const lastDay = new Date(2019, 11, 21);
+const formatted = format(lastDay, 'yyyyMMdd');
+
 const urls = new Map<string, string>([
   [
     'thewhitehorseinn.com',
-    'https://thewhitehorseinn.checkfront.com/reserve/?inline=1&category_id=2&date=20191221&options=tabs&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fwww.thewhitehorseinn.com&1574728158411#D20191221',
+    `https://thewhitehorseinn.checkfront.com/reserve/?inline=1&category_id=2&date=${formatted}&options=tabs&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fwww.thewhitehorseinn.com&1574728158411#D${formatted}`,
   ],
   [
     'campticonderoga.com',
-    'https://campticonderoga.checkfront.com/reserve/?inline=1&date=20191221&options=category_select&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fcampticonderoga.com&1574626041874#',
+    `https://campticonderoga.checkfront.com/reserve/?inline=1&date=${formatted}&options=category_select&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fcampticonderoga.com&1574626041874#${formatted}`,
   ],
 ]);
 
@@ -26,16 +30,15 @@ const isDev = process.env.NOW_REGION === 'dev1';
 const IglooChecker = async (req: NowRequest, res: NowResponse) => {
   const url =
     urls.get(Array.isArray(req.query.url) ? req.query.url[0] : req.query.url) ??
-    'https://campticonderoga.checkfront.com/reserve/?inline=1&date=20191221&options=category_select&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fcampticonderoga.com&1574626041874#';
+    `https://campticonderoga.checkfront.com/reserve/?inline=1&date=${formatted}&options=category_select&style=font-family%3A%20tahoma&provider=droplet&ssl=1&src=https%3A%2F%2Fcampticonderoga.com&1574626041874#${formatted}`;
 
   const now = new Date();
-  const lastDay = new Date(2019, 11, 21);
   const date = lastDay.toLocaleDateString('en', {
     month: 'long',
     day: 'numeric',
   });
 
-  if (now > lastDay) {
+  if (isAfter(now, lastDay)) {
     return res.json({ message: 'date is in the past, sadness..' });
   }
 
@@ -43,9 +46,9 @@ const IglooChecker = async (req: NowRequest, res: NowResponse) => {
   const browser = await launch(options);
   const page = await browser.newPage();
 
-  await page.goto(url);
+  await page.goto(url, { waitUntil: 'networkidle2' });
 
-  const available = await page.$('.cf-item-status.AVAILABLE');
+  const available = !!(await page.$('.cf-item-status.AVAILABLE'));
 
   if (available) {
     const promises = [];
